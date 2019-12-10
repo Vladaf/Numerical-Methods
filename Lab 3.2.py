4981314045c43e6cc00952dc083079495613cc02
Vladaf <vladaf1999@tut.by>
2019-12-10 19:50:19 +0300

    3.2

---
+++ Lab 3.2.py
@@ -0,0 +1,33 @@
+import math
+import random
+
+def f(x)
+    return 4.0  math.sin(x) + 1.0 - x
+
+def secant_method(g, x0, x1, eps)
+    iters = 0 
+    while True
+        f0 = g(x0)
+        f1 = g(x1)
+        xn = x1 - (x1 - x0)  f1  (f1 - f0)
+        if (abs(x1 - xn)  eps)
+            return xn
+        x0 = x1
+        x1 = xn
+        iters += 1
+        if (iters  100)
+            return float('NaN')
+
+random.seed(300) 
+
+roots = []
+for i in range(50)
+    r1 = random.uniform(-10, 10)
+    r2 = random.uniform(-10, 10)
+    sol = secant_method(f, r1, r2, 1e-9)
+    if not(any(abs(x - sol)  1e-9 for x in roots))
+        roots.append(sol)
+    
+roots.sort()
+
+print(roots)
