eb40da3b89bf95905dca60f4bd33f1bd597a9172
Vladaf <vladaf1999@tut.by>
2019-12-10 20:19:16 +0300

    bug

--- Lab 3.2.py
+++ Lab 3.2.py
@@ -1,31 +1,31 @@
 import math
 import random
 
-def f(x)
-    return 4.0  math.sin(x) + 1.0 - x
+def f(x):
+    return 4.0 * math.sin(x) + 1.0 - x
 
-def secant_method(g, x0, x1, eps)
+def secant_method(g, x0, x1, eps):
     iters = 0 
-    while True
+    while True:
         f0 = g(x0)
         f1 = g(x1)
-        xn = x1 - (x1 - x0)  f1  (f1 - f0)
-        if (abs(x1 - xn)  eps)
+        xn = x1 - (x1 - x0) * f1 / (f1 - f0)
+        if (abs(x1 - xn) < eps):
             return xn
         x0 = x1
         x1 = xn
         iters += 1
-        if (iters  100)
+        if (iters > 100):
             return float('NaN')
 
-random.seed(300) 
+random.seed(1223) 
 
 roots = []
-for i in range(50)
+for i in range(50):
     r1 = random.uniform(-10, 10)
     r2 = random.uniform(-10, 10)
     sol = secant_method(f, r1, r2, 1e-9)
-    if not(any(abs(x - sol)  1e-9 for x in roots))
+    if not(any(abs(x - sol) < 1e-9 for x in roots)):
         roots.append(sol)
     
 roots.sort()
